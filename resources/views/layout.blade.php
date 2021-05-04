a489db8c5210da6559e38b3354a2dfda3b448c6f
nt0412 <namthuan412@gmail.com>
2021-05-04 20:13:15 +0700

    xongnavbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -20,32 +20,29 @@
             background-color: black;
         }
 
-        .topnav {
-            overflow: hidden;
-            background-color: #333;
-        }
-
-        .topnav a {
-            float: left;
-            color: #f2f2f2;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-            font-size: 17px;
-        }
-
-        .topnav a:hover {
-            background-color: #ddd;
-            color: black;
-        }
-
-        .topnav a.active {
-            background-color: #04AA6D;
+        #navbarNav a {
             color: white;
+            text-decoration: none;
+            font-weight: bold;
+            font-size: larger;
         }
 
-        .topnav-right {
-            float: right;
+        #navbarNav a:hover {
+            color: #b973ff;
+            text-decoration: #b973ff;
+        }
+
+        .dropdown-menu {
+            background-color: #b973ff;
+        }
+
+        .dropdown-menu a:hover {
+            background-color: black;
+            color: #b973ff;
+        }
+
+        .dropdown-menu{
+            transition: 1s;
         }
     </style>
 </head>
@@ -63,104 +60,105 @@
         <div class="collapse navbar-collapse" id="navbarNav" style="transition: 0.4s; color: transparent;">
             <ul class="navbar-nav mr-auto">
                 <li class='nav-item dropdown'>
-                    <a class='nav-link dropdown-toggle' style="color: #b973ff; font-weight: bold; font-size: larger;" href='#' id='navbardrop' data-toggle='dropdown'>
+                    <a class='nav-link dropdown-toggle' href='#' id='navbardrop' data-toggle='dropdown'>
                         Tech
                     </a>
                     <div class='dropdown-menu' style='text-align: center;'>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Apple
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Samsung
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Xiaomi
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Sony
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Asus
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Nintendo
                         </a>
                     </div>
                 </li>
                 <li class='nav-item dropdown'>
-                    <a class='nav-link dropdown-toggle' style="color: #b973ff; font-weight: bold; font-size: larger;" href='#' id='navbardrop' data-toggle='dropdown'>
+                    <a class='nav-link dropdown-toggle' href='#' id='navbardrop' data-toggle='dropdown'>
                         Reviews
                     </a>
                     <div class='dropdown-menu' style='text-align: center;'>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Phone
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Tablet
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Laptop
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Watch
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             PC
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Playstation
                         </a>
                     </div>
                 </li>
                 <li class='nav-item dropdown'>
-                    <a class='nav-link dropdown-toggle' style="color: #b973ff; font-weight: bold; font-size: larger;" href='#' id='navbardrop' data-toggle='dropdown'>
+                    <a class='nav-link dropdown-toggle' href='#' id='navbardrop' data-toggle='dropdown'>
                         Entertainment
                     </a>
                     <div class='dropdown-menu' style='text-align: center;'>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Film
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             TV
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Games
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Comics
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Books
                         </a>
                     </div>
                 </li>
                 <li class='nav-item dropdown'>
-                    <a class='nav-link dropdown-toggle' style="color: #b973ff; font-weight: bold; font-size: larger;" href='#' id='navbardrop' data-toggle='dropdown'>
+                    <a class='nav-link dropdown-toggle' href='#' id='navbardrop' data-toggle='dropdown'>
                         About us
                     </a>
                     <div class='dropdown-menu' style='text-align: center;'>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Facebook
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Instagram
                         </a>
-                        <a style='color: black;' class='nav-link' href='#'>
+                        <a class='nav-link' href='#'>
                             Twitter
                         </a>
                     </div>
                 </li>
             </ul>
+
             <ul class="navbar-nav">
                 <li class='nav-item dropdown'>
                     <a class='nav-link dropdown-toggle' href='#' id='navbardrop' data-toggle='dropdown'>
-                        <img src='../../../Convouless/image/navbar_icon/outline_account_circle_white_24dp.png'>  
+                        <img src='../../../Convouless/image/navbar_icon/outline_account_circle_white_24dp.png'>
                     </a>
                     <div class='dropdown-menu' style='text-align: center;'>
-                        <a style='color: black;' class='nav-link' href='../../LAB3/php/login.php'>
+                        <a class='nav-link' href='../../LAB3/php/login.php'>
                             <img src='../../../Convouless/image/_login.png' class='img-fluid'>Login
                         </a>
-                        <a style='color: black;' class='nav-link' href='../../LAB3/php/register.php'>
+                        <a class='nav-link' href='../../LAB3/php/register.php'>
                             <img src='../../../Convouless/image/_register.png' class='img-fluid'>Register
                         </a>
                     </div>

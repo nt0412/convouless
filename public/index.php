701b908e7c051b8309faf5dd52439b5b186673ca
nt0412 <namthuan412@gmail.com>
2021-05-04 09:20:04 +0700

    xoafuckyou

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-fuck you
+
 if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
     require __DIR__.'/../storage/framework/maintenance.php';
 }

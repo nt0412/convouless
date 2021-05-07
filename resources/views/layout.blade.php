933e968d3ed5dd1188850b2e7976c5d876838cd5
ledangtuankhoi <ledangtuankhoi@gmail.com>
2021-05-07 09:56:21 +0700

    test fist

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -33,6 +33,322 @@
         </div>
     </div>
 
-    
+
+</div>
+<div class="container-fluid ">
+    <div class="conten">
+
+        <!-- header -->
+        <style>
+            .container-fluid {
+                /* transition: .5; */
+                text-decoration: none;
+                list-style: none;
+                background-color: black;
+                color: black;
+            }
+
+            .container {
+                background-color: white !important;
+            }
+
+            a {
+                text-decoration: none;
+                color: gray;
+            }
+
+            ul li {
+                list-style: none;
+            }
+
+            a:hover {
+                color: red;
+            }
+
+            .noibat {}
+
+            .noibat .row {
+                /* border-color: transparent; */
+                /* max-width: 250px; */
+                /* padding: 1rem; */
+                /* position: relative; */
+                background: linear-gradient(90deg, #f7c626 15%, #f68c2f 40%, #e5127d 85%);
+                /* padding: 3px; */
+            }
+
+            .noibat .card {
+                margin: 3px;
+            }
+
+            .material-icons :hover {
+                border-color: #b973ff;
+            }
+
+            .news_items {
+                margin: 16px;
+                border-top: 2px solid #e0e0e0;
+            }
+
+            .news_items .by {
+                margin: 0 10px;
+            }
+
+            .news_items_noibat {
+                margin: 16px 0;
+                border-top: 2px solid #e0e0e0;
+            }
+
+            .news_items_noibat .title {
+                font-family: 'Krona One', sans-serif;
+                font-size: 30px;
+                color: red;
+                text-transform: uppercase;
+                font-style: italic;
+            }
+
+            .news_items_noibat2 image {
+                height: 500px;
+                overflow: hidden;
+            }
+        </style>
+
+        <img src="{{url('/images/')}}" alt="Image" />
+        <div class="container">
+
+            <div class="noibat my-5 ">
+                <div class="row ">
+
+                    <div class="col-6 p-0">
+                        <div class="card bg-dark text-white" style="">
+                            <img src="./img/apple-watch-s6-44mm-vien-nhom-day-cao-su-navy-232420-102444-600x600.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-family: nunito;font-weight: bold;text-transform: uppercase;">Card title quick example text to build on the card tit</h5>
+                                <p class="card-text"> by <span style="color: red;">some </span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 p-0">
+                        <div class="card bg-dark text-white" style="">
+                            <img src="./img/apple-watch-s6-44mm-vien-nhom-day-cao-su-navy-232420-102444-600x600.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-family: nunito;font-weight: bold;text-transform: uppercase;">Card title quick example text to build on the card tit</h5>
+                                <p class="card-text"> by <span style="color: red;">some </span></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="tinthuong" style="margin: 0 100px;">
+
+                <div class="mota text-center ">
+                    <h3 class="fs-1 text-uppercase ">tech</h3>
+                    <p class="fs-4">The latest tech news about the world's best
+                        (and sometimes worst) hardware, apps, and much more. From
+                        top companies like Google and Apple to tiny startups vying
+                        for your attention, Verge Tech has the latest in what matters in technology daily.</p>
+                </div>
+                <hr>
+                <!-- conten futer -->
+                <div class="news">
+                    <div class="row">
+                        <div class="col-8 ">
+
+
+
+
+                            <div class="news_items_noibat">
+                                <div class="row" style="margin: 16px 0; ">
+                                    <div class="col-6" style="text-align: right;">
+                                        <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem</h4>
+                                    </div>
+                                    <div class="col-6 title">
+                                        <h4>Lorem Ipsum Lorem</h4>
+                                    </div>
+                                    <div class="by">
+                                        by
+                                        <a href="">
+                                            startups
+                                        </a>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+
+
+                            <div class="news_items">
+                                <div class="d-flex justify-content-start">
+                                    <div>
+
+                                        <img src="./img/asus-vivobook-x415ja-i5-ek090t-17-600x600.jpg" width="221px" alt="">
+                                    </div>
+                                    <div>
+                                        <h4>(and sometimes worst) hardware, apps, and much more and much more. From
+                                            top companies like Google and Apple to tiny startups
+                                        </h4>
+                                        <div class=" news_static d-flex justify-content-start">
+                                            <div class="by">
+                                                by
+                                                <a href="">
+                                                    startups
+                                                </a>
+                                            </div>
+                                            <div style="border-left: 1px solid; margin: 0 10px;"></div>
+
+                                            <div class="time">
+                                                time today 12:30
+                                            </div>
+                                            <div style="border-left: 1px solid; margin: 0 10px;"></div>
+                                            <div class=".comment">
+                                                <a href="">
+                                                    123 comment
+                                                </a>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                            <div class="news_items_noibat2">
+                                <div>
+                                    <div>
+                                        <h4>(and sometimes worst) hardware, apps, and much more and much more. From
+                                            top companies like Google and Apple to tiny startups
+                                        </h4>
+                                        <div class=" news_static d-flex justify-content-start">
+                                            <div class="by">
+                                                by
+                                                <a href="">
+                                                    startups
+                                                </a>
+                                            </div>
+                                            <div style="border-left: 1px solid; margin: 0 10px;"></div>
+
+                                            <div class="time">
+                                                time today 12:30
+                                            </div>
+                                            <div style="border-left: 1px solid; margin: 0 10px;"></div>
+                                            <div class=".comment">
+                                                <a href="">
+                                                    123 comment
+                                                </a>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+
+                                    <div class="image">
+                                        <img src="./img/ipad-gen-8-cellular-vang-new-600x600-600x600.jpg" alt="" style="width: 100%; ">
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+
+
+
+
+                            <div class="news_items_noibat">
+                                <div class="row" style="margin: 16px 0; ">
+                                    <div class="col-6" style="text-align: right;">
+                                        <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem</h4>
+                                    </div>
+                                    <div class="col-6">
+                                        <h4 class="title" style="">Lorem Ipsum Lorem</h4>
+                                    </div>
+                                    <div class="by">
+                                        by
+                                        <a href="">
+                                            startups
+                                        </a>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+
+
+
+
+                        </div>
+                        <!-- video futuer -->
+                        <div class="col-4">
+                            <h4 class="text-center fs-1" style="border-bottom: 2px solid #b973ff ;"> video furnter</h4>
+                            <div class="video_items">
+                                <ul style="padding-left: 0px;">
+                                    <li class="" style="position: relative;">
+                                        <div style="height: 160px; overflow: hidden;">
+
+                                            <img class="img-fluid" src="./img/apple-watch-s6-44mm-vien-nhom-day-cao-su-red-232420-102444-600x600.jpg" alt="">
+                                        </div>
+                                        <!-- <i class="material-icons" >play_arrow</i> -->
+                                        <i class="fas fa-play" style=" border: 7px solid white;position: absolute;top: 35%;font-size: 80px;right: 39%;border-radius: 50%;background: whitesmoke;"></i>
+                                    </li>
+                                    <li>
+                                        <h5>mes worst) hardware, apps, and much more and much more. From
+                                            top companies like Google and Apple to tiny startups</h5>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <div class="video_items">
+                                <ul style="padding-left: 0px;">
+                                    <li class="" style="position: relative;">
+                                        <div style="height: 160px; overflow: hidden;">
+
+                                            <img class="img-fluid" src="./img/apple-watch-s6-44mm-vien-nhom-day-cao-su-red-232420-102444-600x600.jpg" alt="">
+                                        </div>
+                                        <!-- <i class="material-icons" style=" border: 7px solid white;position: absolute;top: 35%;font-size: 80px;right: 39%;border-radius: 50%;background: whitesmoke;">play_arrow</i> -->
+                                        <i class="fas fa-play" style=" border: 7px solid white;position: absolute;top: 35%;font-size: 80px;right: 39%;border-radius: 50%;background: whitesmoke;"></i>
+
+                                    </li>
+                                    <li>
+                                        <h5>mes worst) hardware, apps, and much more and much more. From
+                                            top companies like Google and Apple to tiny startups</h5>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <div class="video_items">
+                                <ul style="padding-left: 0px;">
+                                    <li class="" style="position: relative;">
+                                        <div style="height: 160px; overflow: hidden;">
+
+                                            <img class="img-fluid" src="./img/apple-watch-s6-44mm-vien-nhom-day-cao-su-red-232420-102444-600x600.jpg" alt="">
+                                        </div>
+                                        <!-- <i class="material-icons" style=" border: 7px solid white;position: absolute;top: 35%;font-size: 80px;right: 39%;border-radius: 50%;background: whitesmoke;">play_arrow</i> -->
+                                        <i class="fas fa-play" style=" border: 7px solid white;position: absolute;top: 35%;font-size: 80px;right: 39%;border-radius: 50%;background: whitesmoke;"></i>
+
+                                    </li>
+                                    <li>
+                                        <h5>mes worst) hardware, apps, and much more and much more. From
+                                            top companies like Google and Apple to tiny startups</h5>
+                                    </li>
+                                </ul>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+            </div>
+        </div>
+
+
+
+    </div>
 </div>
 <?php include_once("../Convouless/php/footer.php"); ?>
